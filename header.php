727d375057222bf21bd68d4d525badaa8810ec01
LaelatulHodijah <hodijah2208@gmail.com>
2022-12-08 08:21:14 +0700

    Update header.php

--- header.php
+++ header.php
@@ -9,9 +9,9 @@
 					
 				<!--start: Logo -->
 				<div class="logo span3">
-					<center>
-					<a class="brand" href="index.php"><img src="img/logo.km.jpeg" alt="Logo"></a>
-</center>
+					
+					<a class="brand" href="index.php"><img src="img/logo4.jpeg" alt="Logo"></a>
+
 				</div>
 				<!--end: Logo -->
 					
@@ -42,6 +42,111 @@
 			                			<ul class="dropdown-menu">
 			                  				<li><a href="login.html">Admin</a></li>
 			                  				<li><a href="index.html">Customer</a></li>
+											  <head>
+<style>
+	/* UTILITIES */
+
+* {
+
+margin: 0;
+padding: 0;
+box-sizing: border-box;
+}
+
+body {
+font-family: cursive;
+}
+
+a {
+text-decoration: none;
+}
+
+li {
+list-style: none;
+}
+.navbar {
+display: flex;
+align-items: center;
+justify-content: space-between;
+padding: 20px;
+background-color: teal;
+color: #fff;
+}
+
+.nav-links a {
+color: #fff;
+}
+/* LOGO */
+.logo {
+
+font-size: 32px;
+
+}
+
+/* NAVBAR MENU */
+
+.menu {
+
+display: flex;
+
+gap: 1em;
+
+font-size: 18px;
+
+}
+
+.menu li:hover {
+
+background-color: #4c9e9e;
+
+border-radius: 5px;
+
+transition: 0.3s ease;
+
+}
+
+.menu li {
+
+padding: 5px 14px;
+
+}
+
+/* DROPDOWN MENU */
+
+.services {
+
+position: relative;
+
+}
+
+.dropdown {
+background-color: rgb(1, 139, 139);
+padding: 1em 0;
+position: absolute; /*WITH RESPECT TO PARENT*/
+display: none;
+border-radius: 8px;
+top: 35px;
+}
+
+.dropdown li + li {
+margin-top: 10px;
+}
+
+.dropdown li {
+padding: 0.5em 1em;
+width: 8em;
+text-align: center;
+}
+
+.dropdown li:hover {
+background-color: #4c9e9e;
+}
+
+.services:hover .dropdown {
+display: block;
+}
+</style>
+</head>
 			                			</ul>
 			              			</li>
 			            		</ul>
