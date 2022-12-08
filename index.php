aa70911a6df10b1691a2c977b446b7524ed91530
LaelatulHodijah <hodijah2208@gmail.com>
2022-12-08 08:21:28 +0700

    Update index.php

--- index.php
+++ index.php
@@ -147,7 +147,7 @@
 				<!-- start: Footer Menu Logo -->
 				<div class="span2">
 					<div id="footer-menu-logo">
-						<a href="#"><img src="img/logo3.png" alt="logo" /></a>
+						<a href="#"><img src="img/logo4.jpeg" alt="logo" /></a>
 					</div>
 				</div>
 				<!-- end: Footer Menu Logo -->
